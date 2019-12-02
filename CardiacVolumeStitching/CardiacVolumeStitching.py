ceefd7e68a51e3fa2c105e6fe59742630fef1eb2
pcarnah <pcarnah@uwo.ca>
2019-12-02 15:45:22 -0500

    Update copyright

--- CardiacVolumeStitching/CardiacVolumeStitching.py
+++ CardiacVolumeStitching/CardiacVolumeStitching.py
@@ -1,3 +1,9 @@
+#  /*====================================================================
+#  Copyright (C) 2019  Patrick Carnahan <pcarnah@uwo.ca>
+#  Unauthorized copying of this file, via any medium is strictly prohibited
+#  Proprietary and confidential
+#  ====================================================================*/
+
 import gc
 import os
 import unittest
